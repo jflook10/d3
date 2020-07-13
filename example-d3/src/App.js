a7bc6e05d8facf07541dbb741f729a60346e8bbd
Jillian Flook <jkenw10@gmail.com>
2020-07-13 15:34:38 -0500

    ch 1-2, ex rects with json data

---
+++ example-d3/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ChartWrapper from './ChartWrapper'
+function App() {
+  return (
+    <div className="App">
+      <ChartWrapper />
+    </div>
+  );
+}
+
+export default App;
